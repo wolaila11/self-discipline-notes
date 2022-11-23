1e001d3881c3bd0680e761d419998509b99f4d9b
dongdong.wang <dongdong.wang@united-imaging.com>
2022-11-23 17:50:54 +0800

    11

--- 前端/09-react/代码资料/源码/react_staging/19_src_push与replace模式/components/MyNavLink/index.jsx
+++ 前端/09-react/代码资料/源码/react_staging/19_src_push与replace模式/components/MyNavLink/index.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 
 export default class MyNavLink extends Component {
 	render() {
-		// console.log(this.props);
+		
 		return (
 			<NavLink activeClassName="atguigu" className="list-group-item" {...this.props}/>
 		)
